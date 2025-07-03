18f19dd6214636b4208ddd33167b3a39ed3c0ba2
Pablo Ramirez <pabloramirez.dev@gmail.com>
2025-07-03 16:46:11 -0300

    feat: set up database module

---
+++ backend/src/shared/database/typeorm.config.ts
@@ -0,0 +1,11 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const typeOrmConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => ({
+  type: 'postgres',
+  url: configService.get('DB_URL'),
+  entities: [__dirname + '/../../modules/**/*.entity{.ts,.js}'],
+  synchronize: configService.get('NODE_ENV') === 'development',
+});
