82bad0a65a12ebad3e701161153c382b865d865e
Pablo Ramirez <pabloramirez.dev@gmail.com>
2025-07-03 16:57:55 -0300

    feat: log port on app start

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = app.get(Logger);
+
+  await app.listen(PORT, () => {
+    logger.log(`App listening on port ${PORT}`, 'main');
+  });
 }
 bootstrap();
