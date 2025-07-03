2064e99c28809506e1d4f72d1d3a2a09d39b3fa3
Pablo Ramirez <pabloramirez.dev@gmail.com>
2025-07-03 16:57:43 -0300

    feat: add common module with logger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookingsModule } from './modules/bookings/bookings.module';
 import { RoomingListsModule } from './modules/rooming-lists/rooming-lists.module';
 import { DatabaseModule } from './shared/database/database.module';
+import { CommonModule } from './shared/common/common.module';
 
 @Module({
-  imports: [DatabaseModule, BookingsModule, RoomingListsModule],
+  imports: [CommonModule, DatabaseModule, BookingsModule, RoomingListsModule],
   controllers: [],
   providers: [],
 })
